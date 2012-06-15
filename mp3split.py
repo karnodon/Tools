1724c52ecd360f9bfc0eabf84449023f06208f41
karnodon <frostbite3@gmail.com>
2012-06-15 09:21:41 +0000

    Some cosmetics

--- mp3split.py
+++ mp3split.py
@@ -1,17 +1,17 @@
+__author__ = 'mmorozov'
 import os
 import re
 import wave
 import sys
-
-__author__ = 'mmorozov'
 from subprocess import call
+
 class Splitter:
-    def read(self):
+    def make_wav(self):
         try:
             srcpath = sys.argv[1]
             self.number = int(re.search('(\d+)', srcpath).group(0))
             self.dirpath = re.split('[^\\\\]+$', srcpath)[0]
-            self.wavpath = "%(path)srt_podcast%(number)d.wav" % {"path" : self.dirpath, "number" : self.number}
+            self.wavpath = "%(p)srt_podcast%(n)d.wav" % {"p" : self.dirpath, "n" : self.number}
             if not os.path.exists(self.wavpath):
                 call(["lame.exe", "--decode",  srcpath, self.wavpath])
             print "WAVEFORM was generated"
@@ -29,13 +29,13 @@ class Splitter:
         r = 0
         frames_per_file = wr.getframerate() * 60 * 5
         while r * frames_per_file < frame_cnt:
-            frms = wr.readframes(frames_per_file)#5 minutes
-            rtpath = "%(path)s//rt%(idx)d.wav" % {"path": new_dir, "idx": r}
+            frames = wr.readframes(frames_per_file)#5 minutes
+            rtpath = "%(path)s/rt%(idx)d.wav" % {"path": new_dir, "idx": r}
             ww = wave.open(rtpath, "wb")
             ww.setparams(params)
-            ww.writeframes(frms)
+            ww.writeframes(frames)
             ww.close()
-            call(["lame.exe", "--ta", "Radio-T ", "--tl", "Radio-T" + str(self.number), "--tt", "track#" + str(r), "--tn", str(r),  rtpath, "%(path)s//rt%(idx)d.mp3" % {"path": new_dir, "idx" : r}])
+            call(["lame.exe", "--ta", "Radio-T", "--tl", "Radio-T" + str(self.number), "--tt", "track#" + str(r), "--tn", str(r),  rtpath, "%(path)s//rt%(idx)d.mp3" % {"path": new_dir, "idx" : r}])
             os.remove(rtpath)
             r += 1
         wr.close()
@@ -44,6 +44,6 @@ class Splitter:
 
 def main():
     split = Splitter()
-    split.read()
+    split.make_wav()
     split.split()
 main()
