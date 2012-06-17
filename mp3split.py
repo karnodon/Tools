c047af76ed8d9a73c87cfcf5325176a851c75e30
Karnodon <frostbite3@gmail.com>
2012-06-17 20:03:07 +0400

    names updated to match PEP string formats

--- mp3split.py
+++ mp3split.py
@@ -1,4 +1,4 @@
-__author__ = 'mmorozov'
+__author__ = 'Max Morozov'
 import os
 import re
 import wave
@@ -8,20 +8,20 @@ from subprocess import call
 class Splitter:
     def make_wav(self):
         try:
-            srcpath = sys.argv[1]
-            self.number = int(re.search('(\d+)', srcpath).group(0))
-            self.dirpath = re.split('[^\\\\]+$', srcpath)[0]
-            self.wavpath = "%(p)srt_podcast%(n)d.wav" % {"p" : self.dirpath, "n" : self.number}
-            if not os.path.exists(self.wavpath):
-                call(["lame.exe", "-S", "--decode",  srcpath, self.wavpath])
+            src_path = sys.argv[1]
+            self.number = int(re.search('(\d+)', src_path).group(0))
+            self.dir_path = re.split('[^\\\\]+$', src_path)[0]
+            self.wav_path = "{:s}rt_podcast{:d}.wav".format(self.dir_path, self.number)
+            if not os.path.exists(self.wav_path):
+                call(["lame.exe", "-S", "--decode",  src_path, self.wav_path])
         except IndexError:
             print "Usage: mp3split <filename>"
 
     def split(self):
-        new_dir = self.dirpath + "rt" + str(self.number)
+        new_dir = self.dir_path + "rt" + str(self.number)
         if not os.path.exists(new_dir):
             os.mkdir(new_dir)
-        wr = wave.open(self.wavpath)
+        wr = wave.open(self.wav_path)
         params = wr.getparams()
         frame_cnt = wr.getnframes()
         r = 0
@@ -47,17 +47,17 @@ class Splitter:
                 break
             if silence > 500:
                 frames = frames[:-(2 * silence)]
-            rtpath = "%(path)s/rt%(idx)d.wav" % {"path": new_dir, "idx": r}
-            ww = wave.open(rtpath, "wb")
+            rt_path = "{:s}/rt{:d}.wav".format(new_dir, r)
+            ww = wave.open(rt_path, "wb")
             ww.setparams(params)
             ww.writeframes(frames)
             ww.close()
-            call(["lame.exe", "-S", "--ta", "Radio-T", "--tl", "Radio-T" + str(self.number), "--tt", "track#{:02d}".format(r), "--tn", str(r),  rtpath, "{:s}/rt{:02d}.mp3".format(new_dir, r)])
-            os.remove(rtpath)
+            call(["lame.exe", "-S", "--ta", "Radio-T", "--tl", "Radio-T " + str(self.number), "--tt", "track#{:02d}".format(r), "--tn", str(r),  rt_path, "{:s}/rt{:02d}.mp3".format(new_dir, r)])
+            os.remove(rt_path)
             frames_wrote += len(frames)/2 #one frame = 2 bytes
             r += 1
         wr.close()
-        os.remove(self.wavpath)
+        os.remove(self.wav_path)
 
 def main():
     split = Splitter()
